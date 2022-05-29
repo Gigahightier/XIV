3a199c9826ab0cde61f84bdfcfbfc96f4de7e385
user987 <42148163+Gigahightier@users.noreply.github.com>
2022-05-29 10:23:50 +0300

    Update Service.cs

--- XIVSlothCombo/Service.cs
+++ XIVSlothCombo/Service.cs
@@ -25,91 +25,91 @@ namespace XIVSlothComboPlugin
         /// <summary>
         /// Gets or sets the plugin configuration.
         /// </summary>
-        internal static PluginConfiguration Configuration { get; set; } = null!;
+        public static PluginConfiguration Configuration { get; set; } = null!;
 
         /// <summary>
         /// Gets or sets the plugin caching mechanism.
         /// </summary>
-        internal static CustomComboCache ComboCache { get; set; } = null!;
+        public static CustomComboCache ComboCache { get; set; } = null!;
 
         /// <summary>
         /// Gets or sets the plugin icon replacer.
         /// </summary>
-        internal static IconReplacer IconReplacer { get; set; } = null!;
+        public static IconReplacer IconReplacer { get; set; } = null!;
 
         /// <summary>
         /// Gets or sets the plugin address resolver.
         /// </summary>
-        internal static PluginAddressResolver Address { get; set; } = null!;
+        public static PluginAddressResolver Address { get; set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud plugin interface.
         /// </summary>
         [PluginService]
-        internal static DalamudPluginInterface Interface { get; private set; } = null!;
+        public static DalamudPluginInterface Interface { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud buddy list.
         /// </summary>
         [PluginService]
-        internal static BuddyList BuddyList { get; private set; } = null!;
+        public static BuddyList BuddyList { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud chat gui.
         /// </summary>
         [PluginService]
-        internal static ChatGui ChatGui { get; private set; } = null!;
+        public static ChatGui ChatGui { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud client state.
         /// </summary>
         [PluginService]
-        internal static ClientState ClientState { get; private set; } = null!;
+        public static ClientState ClientState { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud command manager.
         /// </summary>
         [PluginService]
-        internal static CommandManager CommandManager { get; private set; } = null!;
+        public static CommandManager CommandManager { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud condition.
         /// </summary>
         [PluginService]
-        internal static Condition Condition { get; private set; } = null!;
+        public static Condition Condition { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud data manager.
         /// </summary>
         [PluginService]
-        internal static DataManager DataManager { get; private set; } = null!;
+        public static DataManager DataManager { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud framework manager.
         /// </summary>
         [PluginService]
-        internal static Framework Framework { get; private set; } = null!;
+        public static Framework Framework { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud job gauges.
         /// </summary>
         [PluginService]
-        internal static JobGauges JobGauges { get; private set; } = null!;
+        public static JobGauges JobGauges { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud object table.
         /// </summary>
         [PluginService]
-        internal static ObjectTable ObjectTable { get; private set; } = null!;
+        public static ObjectTable ObjectTable { get; private set; } = null!;
 
         /// <summary>
         /// Gets the Dalamud target manager.
         /// </summary>
         [PluginService]
-        internal static TargetManager TargetManager { get; private set; } = null!;
+        public static TargetManager TargetManager { get; private set; } = null!;
 
         [PluginService]
-        internal static SigScanner SigScanner { get; private set; } = null!;
+        public static SigScanner SigScanner { get; private set; } = null!;
 
         /// <summary>
         /// Returns the Plugin Folder location
@@ -129,10 +129,10 @@ namespace XIVSlothComboPlugin
         /// Gets the Dalamud party list.
         /// </summary>
         [PluginService]
-        internal static PartyList PartyList { get; private set; } = null!;
+        public static PartyList PartyList { get; private set; } = null!;
 
         [PluginService]
-        internal static GameGui GameGui { get; private set; } = null!;
+        public static GameGui GameGui { get; private set; } = null!;
 
     }
 }
